62bf2b18716e291ed6d8934b4685ea1be4852173
Luis Payan <luis@tremak.com>
2021-04-14 13:54:11 -0600

    Public assets from Proxy fix

--- lib/generators/mints_files_generator.rb
+++ lib/generators/mints_files_generator.rb
@@ -6,8 +6,10 @@ class MintsFilesGenerator < Rails::Generators::Base
       copy_file 'mints_user_controller.rb', './app/controllers/api/mints_user_controller.rb'
       copy_file 'mints_contact_controller.rb', './app/controllers/api/v1/mints_contact_controller.rb'
       copy_file 'mints_public_controller.rb', './app/controllers/api/v1/mints_public_controller.rb'
+      copy_file 'mints_assets_controller.rb', './app/controllers/mints_assets_controller.rb'
       route <<-eos
         # Mints auto-generated routes (proxy to send request to mints.cloud)
+        match '/public-assets/*path' => 'mints_assets#index', via: [:get, :post, :put, :patch, :delete]
         namespace :api, defaults: { format: :json } do
           match '/user/v1/*path' => 'mints_user#index', via: [:get, :post, :put, :patch, :delete]
           match '/contact/v1/*path' => 'mints_contact#index', via: [:get, :post, :put, :patch, :delete]  
